bc1cd9206b05726590a4543bff73d24c5762b922
Seb Dalgarno <seb@poissonconsulting.ca>
2019-07-19 12:05:26 -0700

    add fix functions back

--- R/fix.R
+++ R/fix.R
@@ -1,43 +1,43 @@
-#' # Fix UI
-#' #' @title fix_ui and fix_server
-#' #' @descriptionServer and ui components to generate fix text.
-#' #'
-#' #' @param fix A character string of the fix text.
-#' #' @param condition_name A character string of the condition name or, if add_output_ns = FALSE, JS code evaluating a condition (with no server side component).
-#' #' @param fix_size A character string of the fix text size.
-#' #' @param add_output_ns A flag indicating whether to add output and namespace to condition string.
-#' #' @param ns A function of the namespace.
-#' #' @param ... arguments passed to conditionalPanel
-#' #' @param output internal
-#' #' @param condition A shiny reactive evaluating to a flag.
-#' #'
-#' #' @rdname fix
-#' #'
-#' #' @keywords internal
-#' #' @export
-#' fix_ui <- function(fix, condition_name, ns = NS(NULL), add_output_ns = TRUE, 
-#'                    fix_size = "13px", ...){
-#'   
-#'   check_string(fix)
-#'   check_string(condition_name)
-#'   
-#'   if(isTRUE(add_output_ns))
-#'     condition_name <- paste(glue("output['{ns(condition_name)}']"))
-#'   
-#'   print(condition_name)
-#'   conditionalPanel(
-#'     condition = condition_name,
-#'     fix_text(fix, fix_size), ...
-#'   )
-#' }
-#' 
-#' # Fix Server
-#' 
-#' #' @rdname fix
-#' #' @export
-#' #' @keywords internal
-#' #' 
-#' fix_server <- function(output, condition_name, condition){
-#'   output[[condition_name]] <- condition
-#'   outputOptions(output, condition_name, suspendWhenHidden = FALSE)
-#' }
+# Fix UI
+#' @title fix_ui and fix_server
+#' @descriptionServer and ui components to generate fix text.
+#'
+#' @param fix A character string of the fix text.
+#' @param condition_name A character string of the condition name or, if add_output_ns = FALSE, JS code evaluating a condition (with no server side component).
+#' @param fix_size A character string of the fix text size.
+#' @param add_output_ns A flag indicating whether to add output and namespace to condition string.
+#' @param ns A function of the namespace.
+#' @param ... arguments passed to conditionalPanel
+#' @param output internal
+#' @param condition A shiny reactive evaluating to a flag.
+#'
+#' @rdname fix
+#'
+#' @keywords internal
+#' @export
+fix_ui <- function(fix, condition_name, ns = NS(NULL), add_output_ns = TRUE,
+                   fix_size = "13px", ...){
+
+  check_string(fix)
+  check_string(condition_name)
+
+  if(isTRUE(add_output_ns))
+    condition_name <- paste(glue("output['{ns(condition_name)}']"))
+
+  print(condition_name)
+  conditionalPanel(
+    condition = condition_name,
+    fix_text(fix, fix_size), ...
+  )
+}
+
+# Fix Server
+
+#' @rdname fix
+#' @export
+#' @keywords internal
+#'
+fix_server <- function(output, condition_name, condition){
+  output[[condition_name]] <- condition
+  outputOptions(output, condition_name, suspendWhenHidden = FALSE)
+}
--- tests/testthat/test-css.R
+++ tests/testthat/test-css.R
@@ -3,5 +3,6 @@ test_that("css functions work", {
   expect_is(css_navbar(), "shiny.tag")
   expect_is(css_slider(), "shiny.tag")
   expect_is(css_hide_errors(), "shiny.tag")
+  expect_is(css_leaflet_full(), "shiny.tag")
   expect_is(css_body(), "shiny.tag")
 })
