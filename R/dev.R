7db0b71b3dad3618320b7c34c3737ade8e3a3dea
Seb Dalgarno <seb@poissonconsulting.ca>
2019-09-16 16:58:23 -0700

    add dev_prep function

---
+++ R/dev.R
@@ -0,0 +1,20 @@
+#' Prepare package to run app during development
+#'
+#' @param rm_all A flag indicating whether to remove objects from Global environment.
+#' @param detach_all A flag indicating whether to detach packages.
+#' @return TRUE
+#' @export
+dev_prep <- function(rm_all = TRUE, detach_all = TRUE){
+  chk_flag(rm_all)
+  chk_flag(detach_all)
+  
+  if(rm_all){
+    rm(list=ls(all.names = TRUE))
+  }
+  if(detach_all){
+    all_attached <- paste("package:", names(sessionInfo()$otherPkgs))
+    try(suppressWarnings(lapply(all_attached, detach, character.only = TRUE,
+                                unload = TRUE)), silent = TRUE)
+  }
+  TRUE
+}
