30713f869d22666b3a27982446688b2e682df2a4
Seb Dalgarno <seb@poissonconsulting.ca>
2019-09-26 12:08:43 -0400

    fix dev_prep and add tests

--- R/dev.R
+++ R/dev.R
@@ -1,20 +1,12 @@
 #' Prepare package to run app during development
 #'
-#' @param rm_all A flag indicating whether to remove objects from Global environment.
-#' @param detach_all A flag indicating whether to detach packages.
 #' @return TRUE
 #' @export
-dev_prep <- function(rm_all = TRUE, detach_all = TRUE){
-  chk_flag(rm_all)
-  chk_flag(detach_all)
-  
-  if(rm_all){
-    rm(list=ls(all.names = TRUE))
-  }
-  if(detach_all){
-    all_attached <- paste("package:", names(utils::sessionInfo()$otherPkgs))
-    try(suppressWarnings(lapply(all_attached, detach, character.only = TRUE,
-                                unload = TRUE)), silent = TRUE)
-  }
-  TRUE
+dev_prep <- function(){
+  rm(list=ls(all.names = TRUE, envir = globalenv()), envir = globalenv())
+  all_attached <- paste("package:", names(sessionInfo()$otherPkgs), 
+                        sep = "")
+  try(suppressWarnings(lapply(all_attached, detach, character.only = TRUE, 
+                              unload = TRUE)), silent = TRUE)
+  invisible(TRUE)
 }
--- tests/testthat/test-dev.R
+++ tests/testthat/test-dev.R
@@ -1,3 +1,6 @@
 test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+  x <- 1
+  expect_true(dev_prep())
+  testthat::expect_true(length(ls(all.names = TRUE, envir = globalenv())) == 0)
+  testthat::expect_true(is.null(names(sessionInfo()$otherPkgs)))
 })
